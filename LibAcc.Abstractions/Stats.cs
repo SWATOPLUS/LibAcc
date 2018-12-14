559f05e166f1d9f4459003977ccc7985524a07a4
Pavel Zhdanovich <swatoplus@gmail.com>
2018-12-14 09:53:59 +0300

    I think it's finish

---
+++ LibAcc.Abstractions/Stats.cs
@@ -0,0 +1,11 @@
+﻿namespace LibAcc.Abstractions
+{
+    public class Stats
+    {
+        public int TotalBooks { get; set; }
+
+        public int TotalCustomers { get; set; }
+
+        public int TotalLateRents { get; set; }
+    }
+}
---
+++ LibAcc.Server/Controllers/StatsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using LibAcc.Abstractions;
+using LibAcc.Abstractions.Models;
+using LibAcc.Abstractions.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibAcc.Server.Controllers
+{
+    [Route("api/Stats")]
+    public class StatsController : Controller
+    {
+        private readonly MainDbContext _dbContext;
+
+        public StatsController
+        (
+            MainDbContext dbContext
+        )
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("")]
+        public async Task<Stats> GetAll()
+        {
+            return new Stats
+            {
+                TotalBooks = await _dbContext.Books.CountAsync(),
+                TotalCustomers = await _dbContext.Customers.CountAsync(),
+                TotalLateRents = await _dbContext.RentOrders
+                    .Where(x=> x.OrderDate.AddDays(x.OrderDays) < DateTime.Now)
+                    .CountAsync()
+            };
+        }
+    }
+}
